de1e6c15337e6d7e574ab0fdbbfb24ce46ca701f
Shoan <shoanb2@gmail.com>
2019-10-22 00:20:11 +0530

    Question 1

---
+++ Peak Index In a Mountain Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+        int left=0,right=1;
+
+  
+        while (A[left]<A[right])
+        {
+            left++;
+            right++;
+        }
+        return left;
+    }   
+};
+            
+       
