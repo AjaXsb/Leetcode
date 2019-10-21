c5b06a96d8497a25f0f5e8343477adfbf73e5920
Shoan <shoanb2@gmail.com>
2019-10-22 00:30:36 +0530

    Updated

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,26 +1,99 @@
-int mid;
-int start=0,end=size(nums)-1;
+#include<iostream>
 
-	while (start<=end)
-		{
-			mid=(start+end)/2;
-			if (nums[mid]==target)
-			{
-				return mid;
+using namespace std;
+
+class getnums
+{
+	public:
+	int x,i,y;
+	int nums[100];
+	
+	
+	void getarrn()
+	{	
+		cout<<"Enter No. of elements in the array(less than 100) :"<<endl;
+	    cin>>x;	
+	}
+
+	
+	
+	void getarre()
+	{
+		cout<<"Enter array elements :"<<endl;
+		for (i=0;i<x;i++)
+	        {
+				cin>>nums[i];
 			}
-			else if (nums[mid]>target)
-			{
-				end=mid-1;
-			}
-			else 
-			{
-				start=mid+1;
-			}
-				
-			cout<<nums[0];
 		
+	}
+
+	void gettarget()
+	{
+		cout<<"Enter target no. :"<<endl;
+		cin>>y;
+	}
+	
+	void showarre()
+	{
+		for (i=0;i<x;i++)
+		{
+			cout<<nums[i]<<endl;
+		}
+	}
+	
+	void showtarget()
+	{
+		cout<<"Target is "<<y<<endl;
+	}
+
+};
+
+class check1 : public getnums
+{
+	public:
+		
+		
+		int mid;
+		
+		int check()
+		{
+			int start=0,end=x-1;
+			//cout<<start<<" "<<nums[end]<< " "<<end<<" "<<y<<endl;
+			while (start<=end)
+			{
+				mid=(start+end)/2;
+				if (nums[mid]==y)
+				{
+					return mid;
+				}
+				else if (nums[mid]>y)
+				{
+					end=mid-1;
+				}
+				else 
+				{
+					start=mid+1;
+				}
+				
 			}
 			
 			return -1;
-        
-    		}
+		}
+	};
+	
+int main ()
+{
+	check1 f;
+	f.getarrn();
+	f.getarre();
+	f.gettarget();
+	
+	f.showtarget();
+	cout<<"Target is at position :"<<f.check();
+	return 0;
+}
+			
+	
+	
+		
+	
