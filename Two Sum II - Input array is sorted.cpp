179ccd932caabee567709d3ac69ef1182cc98396
Shoan <shoanb2@gmail.com>
2019-10-22 20:23:38 +0530

    Question 3

---
+++ Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,22 @@
+int i,x=0,y=size(numbers)-1,r;
+        
+        while (numbers[x] + numbers[y] != target)
+        {
+            r=numbers[x]+numbers[y];
+            
+            if (r>target)
+            {
+                y--;
+            }
+            else 
+            {
+                x++;
+            }
+        }
+        x++;
+        y++;
+        vector<int> ans;
+        ans.push_back(x);
+        ans.push_back(y);
+        
+        return ans;
