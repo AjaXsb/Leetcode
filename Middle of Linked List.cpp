7f2b1a81f636a43fc29b5460a6e5dd898d080970
AjaXsb <47176706+AjaXsb@users.noreply.github.com>
2019-11-07 12:47:39 +0530

    Create Middle of Linked List.cpp

---
+++ Middle of Linked List.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    ListNode* middleNode(ListNode* head) {
+
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+
+        if (head != NULL)
+        {
+        while (p2 != NULL && p2->next != NULL)
+        {
+        p2 = p2->next->next;
+        p1 = p1->next;
+        }
+        return p1;
+        }
+        return 0;
+    }
+};
