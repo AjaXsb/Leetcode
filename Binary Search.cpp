419aa537bf5f9abb67ef640981b89a9dbd45624d
AjaXsb <AjaXsb@users.noreply.github.com>
2021-02-03 02:40:16 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,72 +1,17 @@
-#include<iostream>
-
-using namespace std;
-
-class getnums
-{
-	public:
-	int x,i,y;
-	int nums[100];
-	
-	
-	void getarrn()
-	{	
-		cout<<"Enter No. of elements in the array(less than 100) :"<<endl;
-	    cin>>x;	
-	}
-
-	
-	
-	void getarre()
-	{
-		cout<<"Enter array elements :"<<endl;
-		for (i=0;i<x;i++)
-	        {
-				cin>>nums[i];
-			}
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid;
+        int start=0,end=size(nums)-1;
 		
-	}
-
-	void gettarget()
-	{
-		cout<<"Enter target no. :"<<endl;
-		cin>>y;
-	}
-	
-	void showarre()
-	{
-		for (i=0;i<x;i++)
-		{
-			cout<<nums[i]<<endl;
-		}
-	}
-	
-	void showtarget()
-	{
-		cout<<"Target is "<<y<<endl;
-	}
-
-};
-
-class check1 : public getnums
-{
-	public:
-		
-		
-		int mid;
-		
-		int check()
-		{
-			int start=0,end=x-1;
-			//cout<<start<<" "<<nums[end]<< " "<<end<<" "<<y<<endl;
 			while (start<=end)
 			{
 				mid=(start+end)/2;
-				if (nums[mid]==y)
+				if (nums[mid]==target)
 				{
 					return mid;
 				}
-				else if (nums[mid]>y)
+				else if (nums[mid]>target)
 				{
 					end=mid-1;
 				}
@@ -75,25 +20,11 @@ class check1 : public getnums
 					start=mid+1;
 				}
 				
+			cout<<nums[0];
+		
 			}
 			
 			return -1;
-		}
-	};
-	
-int main ()
-{
-	check1 f;
-	f.getarrn();
-	f.getarre();
-	f.gettarget();
-	
-	f.showtarget();
-	cout<<"Target is at position :"<<f.check();
-	return 0;
-}
-			
-	
-	
-		
-	
+        
+    }
+};
